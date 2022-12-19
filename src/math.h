c5507a81caa572ea5900021279bb872f4a29168b
Michał Wiatrowski <wiatrowskimich@gmail.com>
2022-12-19 01:01:09 +0100

    Add generic matrix type

--- src/math.h
+++ src/math.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 struct Vec2 {
     float x;
     float y;
@@ -14,3 +16,26 @@ struct Vec3 {
     float y;
     float z;
 };
+
+template <int Rows, int Cols> struct Matrix : public std::array<float, Rows * Cols> {
+    float &at(int row, int col) { return std::array<float, Rows * Cols>::at(row * Rows + col); }
+    float const &at(int row, int col) const { return std::array<float, Rows * Cols>::at(row * Rows + col); }
+};
+
+template <int A, int B, int C> auto operator*(Matrix<A, B> const &lhs, Matrix<B, C> const &rhs) -> Matrix<A, C> {
+    auto result = Matrix<A, C>{};
+
+    for (int row = 0; row < A; ++row) {
+        for (int col = 0; col < C; ++col) {
+            result.at(row, col) = 0;
+            for (int i = 0; i < B; ++i) {
+                result.at(row, col) += lhs.at(row, i) * rhs.at(i, col);
+            }
+        }
+    }
+
+    return result;
+}
+
+using Vec4 = Matrix<1, 4>;
+using Mat4 = Matrix<4, 4>;
